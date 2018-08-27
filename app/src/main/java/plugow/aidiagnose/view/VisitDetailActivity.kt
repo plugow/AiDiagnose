cb669ce5d73ab3c02ecb569501cf71c825a9438d
Paweł <edypus@gmail.com>
2018-08-27 17:14:51 +0200

    -added visit detail view

---
+++ app/src/main/java/plugow/aidiagnose/view/VisitDetailActivity.kt
@@ -0,0 +1,13 @@
+package plugow.aidiagnose.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import plugow.aidiagnose.R
+
+class VisitDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_visit_detail)
+    }
+}
