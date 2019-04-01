70dd664400023d0ae95f9e22865ebb82899e3811
Paweł <edypus@gmail.com>
2019-04-01 20:22:35 +0200

    -updated splash

--- app/src/main/java/plugow/aidiagnose/view/SplashActivity.kt
+++ app/src/main/java/plugow/aidiagnose/view/SplashActivity.kt
@@ -1,32 +1,44 @@
 package plugow.aidiagnose.view
 
-import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.raizlabs.android.dbflow.config.FlowConfig
 import com.raizlabs.android.dbflow.config.FlowManager
-import org.jetbrains.anko.doAsync
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.schedulers.Schedulers
 import org.jetbrains.anko.startActivity
-import org.jetbrains.anko.uiThread
+import plugow.aidiagnose.useCase.SharedUseCase
+import javax.inject.Inject
 
 class SplashActivity : AppCompatActivity() {
+    @Inject lateinit var sharedUseCase: SharedUseCase
+    private lateinit var disposable:Disposable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         FlowManager.init(FlowConfig.Builder(this).build())
 
-        doAsync {
-            val prefs: SharedPreferences = getSharedPreferences("com.plugow.aidiagnose", MODE_PRIVATE)
-            val isLogged=prefs.getBoolean("isLogged", false)
-
-            uiThread {
-                if (isLogged){
-                    startActivity<NotificationActivity>()
-                }
-                else startActivity<LoginActivity>()
-                finish()
-            }
+        disposable=Single.fromCallable {
+            sharedUseCase.isLogged
         }
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeBy(
+                        onSuccess = {
+                            if (it) startActivity<MainActivity>()
+                            else startActivity<LoginActivity>()
+                        },
+                        onError = {startActivity<LoginActivity>()}
+                )
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        disposable.dispose()
     }
 
 }
